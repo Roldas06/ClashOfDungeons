f7f74741afaaaa45b1f8ca9a0602e02e39be6112
Babloni <harismontvila@gmail.com>
2026-03-09 16:22:23 +0200

    Added aiming system logic

---
+++ Scripts/hand_anchor.gd
@@ -0,0 +1,10 @@
+class_name HandAnchor
+extends Node2D
+#Main script for handling aiming via mouse position
+
+@export var defaultDirection = Vector2.RIGHT
+
+func _process(delta: float) -> void:
+	var cursorPosition = get_global_mouse_position()
+	var rotationAngle = global_position.angle_to_point(cursorPosition)
+	rotation = rotationAngle
