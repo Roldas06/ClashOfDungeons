251122516ed3a80c1b823b646f3d8621427a9783
Enrikas258 <85023131+Enrikas258@users.noreply.github.com>
2026-03-13 16:20:43 +0200

    Fixed lava

--- Scripts/Lava.gd
+++ Scripts/Lava.gd
@@ -3,11 +3,13 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.is_in_lava = true
-		body.speed_multiplier = 5.0 
+		body.speed_multiplier = 5.0
+		body.lava_damage_timer = body.lava_damage_interval  # first hit is instant
 		body.modulate = Color(1, 0.4, 0)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.is_in_lava = false
-		body.speed_multiplier = 30.0 
+		body.speed_multiplier = 30.0
+		body.lava_damage_timer = 0.0
 		body.modulate = Color(1, 1, 1)
