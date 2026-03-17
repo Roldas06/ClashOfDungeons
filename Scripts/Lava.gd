0b27a4f369d2622e653e60ac6b55ff1117c6da5e
Markinum <martynasp8@gmail.com>
2026-03-17 23:16:35 +0200

    Update Lava.gd
    		body.speed_multiplier = 300.0

--- Scripts/Lava.gd
+++ Scripts/Lava.gd
@@ -10,6 +10,6 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.is_in_lava = false
-		body.speed_multiplier = 30.0
+		body.speed_multiplier = 300.0
 		body.lava_damage_timer = 0.0
 		body.modulate = Color(1, 1, 1)
