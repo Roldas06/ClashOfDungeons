0a1de999de0422c04151abb1297e0290f74da816
Markinum <martynasp8@gmail.com>
2026-04-08 22:26:28 +0300

    Enemy Damage indicator
    naudoju enriko damage indicator kad rodytų damage kurį gauna priešai

--- base_enemy.gd
+++ base_enemy.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 @export var speed := 60
 @export var max_hp := 4
 var hp := 4
-
+var damage_number_scene = preload("res://DamageNumber.tscn")
 func _ready():
 	hp = max_hp
 
 func take_damage(amount):
 	hp -= amount
+	var dmg = damage_number_scene.instantiate()
+	get_parent().add_child(dmg)
+	dmg.spawn(amount, global_position + Vector2(0, -20))
 	if hp <= 0:
 		die()
 
